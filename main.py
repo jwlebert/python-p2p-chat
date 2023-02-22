7338acf24f9b209427826e39efc38a644fd38710
JWL4402 <j.lebert4402@gmail.com>
2023-02-22 18:58:23 -0500

    moved into files and got a basic listening server running

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-import socket
+from p2p_chat.peer import Peer
 
-HOST, PORT = '127.0.0.1', 8080
-
-s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-s.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((HOST, PORT))
-s.listen( 5 )
-
-sock, addr = s.accept()
-
-print(sock, addr, sep = "\n")
+peer = Peer()
