6248b4ddb86ce4e255830ae40f226fbe5f05fd9d
JWL <j.lebert4402@gmail.com>
2023-02-25 12:51:39 -0500

    changed indent to tabs and removed prints

--- p2p_chat/keyboard.py
+++ p2p_chat/keyboard.py
@@ -1,19 +1,17 @@
 import threading
 
 class KeyboardThread(threading.Thread):
-    def __init__(self, callback = None, name = "keyboard-thread"):
-        self.callback = callback
-        super(KeyboardThread, self).__init__(name = name)
-        self.start()
+	def __init__(self, callback = None, name = "keyboard-thread"):
+		self.callback = callback
+		super(KeyboardThread, self).__init__(name = name)
+		self.start()
 
-    def run(self):
-        print(True)
-        while True:
-            print(False)
-            # try:
-            self.callback(input())
-            # except KeyboardInterrupt:
-            #     raise
-            #     break
-            # except:
-            #     break
+	def run(self):
+		while True:
+			# try:
+			self.callback(input())
+			# except KeyboardInterrupt:
+			#     raise
+			#     break
+			# except:
+			#     break
