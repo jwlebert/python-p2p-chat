f77e0cf64e70e55175673626c601cf6a34d6eda5
JWL <j.lebert4402@gmail.com>
2023-02-25 15:00:32 -0500

    setup a simple test for sending/receiving with peers

---
+++ test/test_communication.py
@@ -0,0 +1,45 @@
+import unittest
+
+class TestCommunication(unittest.TestCase):
+
+	from p2p_chat.peer import Peer
+    
+	def test_read(self):
+		# Tests basic send/receive functionality. #
+
+		# Initialize two peers on the localhost
+		peer1 = self.Peer('127.0.0.1', 12001)
+		peer2 = self.Peer('127.0.0.1', 12002)
+
+		### Check sending peer1 -> peer2
+		peer1.send_data('127.0.0.1', 12002, "readHello, world!")
+
+		while peer2.latest_request == None:
+			peer2.await_peers()
+
+		command, data = peer2.latest_request
+		peer2.latest_request = None # clear
+
+		self.assertEqual(command, 'READ')
+		self.assertEqual(data, "Hello, world!")
+
+		### Check sending peer2 -> peer1
+
+		peer2.send_data('127.0.0.1', 12001, "readNice to meet you.")
+
+		while peer1.latest_request == None:
+			peer1.await_peers()
+		
+		command, data = peer1.latest_request
+		peer1.latest_request = None # clear
+
+		self.assertEqual(command, 'READ')
+		self.assertEqual(data, "Nice to meet you.")
+
+		peer1.close()
+		peer2.close()
+
+	# TODO test_networking
+
+if __name__ == '__main__':
+	unittest.main()
