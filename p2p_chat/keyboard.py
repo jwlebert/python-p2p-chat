4c1b6c736a26cf64f4ecfac8d3098b72bc543426
JWL <j.lebert4402@gmail.com>
2023-02-25 14:47:00 -0500

    KeyboardThread can now be enabled/disabled

--- p2p_chat/keyboard.py
+++ p2p_chat/keyboard.py
@@ -2,16 +2,11 @@ import threading
 
 class KeyboardThread(threading.Thread):
 	def __init__(self, callback = None, name = "keyboard-thread"):
+		self.alive = True
 		self.callback = callback
 		super(KeyboardThread, self).__init__(name = name)
 		self.start()
 
 	def run(self):
-		while True:
-			# try:
+		while self.alive:
 			self.callback(input())
-			# except KeyboardInterrupt:
-			#     raise
-			#     break
-			# except:
-			#     break
