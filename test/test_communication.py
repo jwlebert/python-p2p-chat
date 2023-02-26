c8670888f28a28174045e8018f730ea1b4714e8d
JWL4402 <j.lebert4402@gmail.com>
2023-02-26 15:42:00 -0500

    changed to addr (host, port), updated documentation, added command param to peer.senddata()

--- test/test_communication.py
+++ test/test_communication.py
@@ -7,12 +7,15 @@ class TestCommunication(unittest.TestCase):
 	def test_read(self):
 		# Tests basic send/receive functionality. #
 
+		addr1 = ('127.0.0.1', 12000)
+		addr2 = ('127.0.0.1', 12001)
+
 		# Initialize two peers on the localhost
-		peer1 = self.Peer('127.0.0.1', 12001, key_input=False)
-		peer2 = self.Peer('127.0.0.1', 12002, key_input=False)
+		peer1 = self.Peer(addr1, key_input=False)
+		peer2 = self.Peer(addr2, key_input=False)
 
 		### Check sending peer1 -> peer2
-		peer1.send_data('127.0.0.1', 12002, "readHello, world!")
+		peer1.send_data(addr2, "read", "Hello, world!")
 
 		while peer2.latest_request == None: pass # wait for data
 
@@ -23,7 +26,7 @@ class TestCommunication(unittest.TestCase):
 		self.assertEqual(data, "Hello, world!")
 
 		### Check sending peer2 -> peer1
-		peer2.send_data('127.0.0.1', 12001, "readNice to meet you.")
+		peer2.send_data(addr1, "read", "Nice to meet you.")
 
 		while peer1.latest_request == None: pass # wait for data
 		
