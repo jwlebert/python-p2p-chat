890795bd1b22d8fa10f9316623d065536efc1f41
JWL4402 <j.lebert4402@gmail.com>
2023-02-26 15:48:40 -0500

    changed key_input to true because it's now off by default

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from p2p_chat.peer import Peer
 host = input("host : ")
 port = int(input("port : "))
 
-peer = Peer(port=port)
+peer = Peer(port=port, key_input=True)
 
 # THIS CODE WILL STILL EXECUTED (NOT-BLOCKED) #
